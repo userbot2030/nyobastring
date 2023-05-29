667650dbbbc6b0b37002bc3817c4ac2c6dd7629e
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-30 04:07:20 +0700

    update env.py

--- env.py
+++ env.py
@@ -1,13 +1,15 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv("config.env")
+load_dotenv()
 
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "postgres://dnwmrnvt:mS08UYFor_mMH8333zNaWX-fWOf28m51@hansken.db.elephantsql.com/dnwmrnvt").strip() # Not a necessary variable anymore but you can add to get stats
-MUST_JOIN = os.getenv("MUST_JOIN", "kynansupport")
+API_ID = int(getenv("API_ID", "")
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+
+MUST_JOIN = getenv("MUST_JOIN", "kynansupport")
+MONGO_URL = getenv("MONGO_URL")
+
 
 if not API_ID:
     raise SystemExit("No API_ID found. Exiting...")
@@ -15,16 +17,8 @@ elif not API_HASH:
     raise SystemExit("No API_HASH found. Exiting...")
 elif not BOT_TOKEN:
     raise SystemExit("No BOT_TOKEN found. Exiting...")
-'''
-if not DATABASE_URL:
-    print("No DATABASE_URL found. Exiting...")
-    raise SystemExit
-'''
 
-try:
-    API_ID = int(API_ID)
-except ValueError:
-    raise SystemExit("API_ID is not a valid integer. Exiting...")
+
 
 if 'postgres' in DATABASE_URL and 'postgresql' not in DATABASE_URL:
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
