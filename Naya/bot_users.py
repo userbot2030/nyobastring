614488fde7cc4717b25156c88523c0756e6ca313
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-30 04:44:47 +0700

    update bot_users.py

--- Naya/bot_users.py
+++ Naya/bot_users.py
@@ -14,3 +14,37 @@ async def users_sql(_, msg: Message):
 async def _stats(_, msg: Message):
     users = len(await get_served_users())
     await msg.reply_text(f"» Kumpulan babi babi liar :\n\nTotal ada {users} babi ")
+
+
+@Client.on_message(filters.command("bacot"))
+async def bacot(client, message):
+    if len(message.command) > 1:
+        text = " ".join(message.command[1:])
+    elif message.reply_to_message is not None:
+        text = message.reply_to_message.text
+    else:
+        await message.reply(
+            "<code>Silakan sertakan pesan atau balas pesan yang ingin disiarkan.</code>"
+        )
+        return
+      
+    if message.from_user.id not in GUA:
+        await message.reply_text(
+            "<b>Lu siapa Monyeddd.</b>"
+        )
+        return
+      
+    babi = await get_served_users()
+
+    total_users = len(babi)
+    sent_count = 0
+    for x in babi:
+        try:
+            await bot.send_message(chat_id=x["id"], text=text)
+            sent_count += 1
+        except Exception as e:
+            await message.reply(f"Error saat mengirim pesan ke {x['id']}: {str(e)}")
+
+    return await message.reply_text(
+        f"<b>Pesan siaran berhasil dikirim kepada {sent_count} babi dari {total_users} babi.</b>"
+    )
