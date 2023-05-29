fdaaf2a16b0c992da368e2fb95a63dc243216f10
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-30 04:11:22 +0700

    update users_sql.py

--- Naya/database/users_sql.py
+++ Naya/database/users_sql.py
@@ -1,43 +1,24 @@
-from env import DATABASE_URL
+from . import db
 
-from sqlalchemy import Column, BigInteger
-
-if DATABASE_URL !="":
-    from Naya.database import BASE, SESSION
-else:
-    BASE = object
+usersdb = db.users
 
 
-class Users(BASE):
-    __tablename__ = "users"
-    __table_args__ = {'extend_existing': True}
-    user_id = Column(BigInteger, primary_key=True)
-
-    def __init__(self, user_id, channels=None):
-        if DATABASE_URL == "":
-            return
-        self.user_id = user_id
-        self.channels = channels
-
-    # def __repr__(self):
-    #     return "<User {} {} {} ({})>".format(self.thumbnail, self.thumbnail_status, self.video_to, self.user_id)
+async def is_served_user(user_id: int) -> bool:
+    user = await usersdb.find_one({"user_id": user_id})
+    if not user:
+        return False
+    return True
 
 
-if DATABASE_URL !="":
-    Users.__table__.create(checkfirst=True)
+async def get_served_users() -> list:
+    users_list = []
+    async for user in usersdb.find({"user_id": {"$gt": 0}}):
+        users_list.append(user)
+    return users_list
 
 
-async def num_users():
-    if DATABASE_URL !="":
-        try:
-            return SESSION.query(Users).count()
-        finally:
-            SESSION.close()
-
-async def sempak():
-    if DATABASE_URL !="":
-        try:
-            users = SESSION.query(Users).all()
-            return [user for user in users]
-        finally:
-            SESSION.close()
+async def add_served_user(user_id: int):
+    is_served = await is_served_user(user_id)
+    if is_served:
+        return
+    return await usersdb.insert_one({"user_id": user_id})
