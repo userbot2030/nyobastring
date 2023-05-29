e818fce9ef506d7babc529ca20cc6cdc87297acd
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-30 06:49:41 +0700

    update generate.py

--- Naya/generate.py
+++ Naya/generate.py
@@ -2,7 +2,7 @@ import asyncio
 from telethon import TelegramClient
 from pyrogram.types import Message
 from pyrogram import Client, filters
-from pyrogram1 import Client as Client1
+from pyrogram import Client as Client1
 from asyncio.exceptions import TimeoutError
 from telethon.sessions import StringSession
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
