79eba880afbde871d6c56811c83c5dd2ff7c5aee
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-30 05:22:47 +0700

    update bot_users.py

--- Naya/bot_users.py
+++ Naya/bot_users.py
@@ -38,9 +38,9 @@ async def bacot(bot: Client, message):
     total_users = len(babi)
     sent_count = 0
     for x in babi:
-    try:
-        await bot.send_message(x, text)
-        sent_count += 1
-    except Exception as e:
-        await message.reply(f"Error saat mengirim pesan ke {x} babi, Karena: {e}")
+        try:
+            await bot.send_message(x, text)
+            sent_count += 1
+        except Exception as e:
+            await message.reply(f"Error saat mengirim pesan ke {x} babi, Karena: {e}")
     return await message.reply(f"Berhasil memotong {sent_count} babi, dari {total_users} babi.")
