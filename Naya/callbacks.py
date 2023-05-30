d89b06c34f7a06abb99f6045bba7a0ea4a7cf473
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-30 20:21:39 +0700

    update callbacks.py

--- Naya/callbacks.py
+++ Naya/callbacks.py
@@ -12,7 +12,6 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
     user = await bot.get_me()
     # user_id = callback_query.from_user.id
     mention = user.mention
-    query = callback_query.data.lower()
     if query.startswith("home"):
         if query == 'home':
             chat_id = callback_query.from_user.id
