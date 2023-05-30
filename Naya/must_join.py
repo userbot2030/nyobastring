05b2a51ce409b5a40d6e61ff3506987f9816ed29
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-30 20:32:11 +0700

    update must_join.py

--- Naya/must_join.py
+++ Naya/must_join.py
@@ -5,8 +5,8 @@ from pyrogram.errors import *
 
 @Client.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-#    if not MUST_JOIN:
-#        return
+    if not MUST_JOIN:
+        return
     try:
         try:
             await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
