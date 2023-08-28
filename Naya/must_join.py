1b37e2a16797dfce60fbb312a45ecc1a269ca825
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-08-28 20:17:43 +0700

    update must_join.py

--- Naya/must_join.py
+++ Naya/must_join.py
@@ -3,8 +3,6 @@ from pyrogram import Client, filters
 from pyrogram.types import *
 from pyrogram.errors import *
 
-MUST_JOIN = "https://t.me/kazusupportgrp"
-
 @Client.on_message(filters.incoming & filters.private, group=1)
 async def must_join_channel(bot: Client, msg: Message):
     #if UserBannedInChannel:
