6e1ff8b04247e687650ef64bed310231f54cbe4e
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-08-28 20:15:50 +0700

    update config.py

--- config.py
+++ config.py
@@ -6,9 +6,14 @@ load_dotenv()
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-
+OWNER_ID = getenv("OWNER_ID", "")
 MUST_JOIN = getenv("MUST_JOIN", "")
+SUPPORT = getenv("SUPPORT", "")
 MONGO_URL = getenv("MONGO_URL", "")
+JOIN1 = getenv("JOIN1", "")
+JOIN2 = getenv("JOIN2", "")
+JOIN3 = getenv("JOIN3", "")
+JOIN4 = getenv("JOIN4", "")
 
 
 if not API_ID:
